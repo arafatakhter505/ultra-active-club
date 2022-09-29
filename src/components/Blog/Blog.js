3ed4cea0ac2e9f39606b3219803c78e60a6efb95
Arafat Akhter <arafatakhterforhad@gmail.com>
2022-09-29 12:10:41 +0600

    blogs added

--- src/components/Blog/Blog.js
+++ src/components/Blog/Blog.js
@@ -10,12 +10,12 @@ const Blog = () => {
         body="ReactJS divides the UI into isolated reusable pieces of code known as components. React components work similarly to JavaScript functions as they accept arbitrary inputs called properties or props. It's possible to have as many components as necessary without cluttering your code."
       ></BlogItem>
       <BlogItem
-        title="How does react work"
-        body="ReactJS divides the UI into isolated reusable pieces of code known as components. React components work similarly to JavaScript functions as they accept arbitrary inputs called properties or props. It's possible to have as many components as necessary without cluttering your code."
+        title="Difference between props and state"
+        body="Props are used to pass data from one component to another. The state is a local data storage that is local to the component only and cannot be passed to other components. The this. setState property is used to update the state values in the component."
       ></BlogItem>
       <BlogItem
-        title="How does react work"
-        body="ReactJS divides the UI into isolated reusable pieces of code known as components. React components work similarly to JavaScript functions as they accept arbitrary inputs called properties or props. It's possible to have as many components as necessary without cluttering your code."
+        title="What works without useEffect data load"
+        body="You respond that your component needs to do something after rendering. React will remember the function you pass. Example: Fetching data from any api, Updating the DOM, Timer."
       ></BlogItem>
     </div>
   );
