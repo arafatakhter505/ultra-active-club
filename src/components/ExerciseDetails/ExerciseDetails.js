69ba051adf941eb3de92199c68f83c46da724415
Arafat Akhter <arafatakhterforhad@gmail.com>
2022-09-29 11:30:32 +0600

    toast pakage install

--- src/components/ExerciseDetails/ExerciseDetails.js
+++ src/components/ExerciseDetails/ExerciseDetails.js
@@ -3,12 +3,18 @@ import "./ExerciseDetails.css";
 
 const ExerciseDetails = (props) => {
   const { exerciseTime, breakTime } = props;
+  const handleActivity = () => {
+    alert("Hey Bro");
+  };
+
   return (
     <div>
       <h3>Exercise Details</h3>
       <p>Exercise Time: {exerciseTime} Seconds</p>
       <p>Break Time: {breakTime} Seconds</p>
-      <button className="activity-btn">Activity Completed</button>
+      <button onClick={handleActivity} className="activity-btn">
+        Activity Completed
+      </button>
     </div>
   );
 };
